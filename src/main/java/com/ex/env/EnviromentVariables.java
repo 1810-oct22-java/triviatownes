114824b4d4394b010ac6f5aa1c2a860b7e2749aa
Joseph Ross <joseph.ross.11211@gmail.com>
2018-12-01 21:50:27 -0500

    test

--- src/main/java/com/ex/env/EnviromentVariables.java
+++
@@ -1,8 +0,0 @@
-package com.ex.env;
-
-public class EnviromentVariables {
-	
-	//Example: You will need to change this to match your own system path to the database.properties file
-	static String dbPropertiesPath = "..../database.properties";
-
-}
