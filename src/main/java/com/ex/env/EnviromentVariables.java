9e7522c3d313159acc345e05ea62ed84ae9ca465
jross11211 <44367789+jross11211@users.noreply.github.com>
2018-12-02 16:56:41 -0500

    Delete Env Vavrs

--- src/main/java/com/ex/env/EnviromentVariables.java
+++
@@ -1,8 +0,0 @@
-package com.ex.env;
-
-public class EnviromentVariables {
-	
-	//Example: You will need to change this to match your own system path to the database.properties file
-	static String dbPropertiesPath = "C:/Users/Owner/my_git_repos/1810-oct22/triviatownes/triviatownes/src/main/java/resources/database.properties";
-
-}
