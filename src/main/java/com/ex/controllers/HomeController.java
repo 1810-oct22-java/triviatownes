187732f5f7f761afece5fcab95a0036388fcf79f
Joseph Ross <joseph.ross.11211@gmail.com>
2018-12-07 13:07:54 -0500

    pushing

---
+++ src/main/java/com/ex/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.ex.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	@ResponseBody
+	public String home() {
+		return "Welcome to Spring!";
+	}
+
+}
