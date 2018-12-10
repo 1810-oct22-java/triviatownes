995e780442b67aea38a1854982530145e673b696
Thien <thienxuanle@yahoo.com>
2018-12-10 10:20:16 -0500

    fix json

--- src/main/java/com/ex/beans/game/ObjectJSON.java
+++ src/main/java/com/ex/beans/game/ObjectJSON.java
@@ -29,19 +29,5 @@ public class ObjectJSON {
 	
 	
 	
-//	public Results[] getResults() {
-//		return results;
-//	}
-//	public void setResults(Results[] results) {
-//		this.results = results;
-//	}
-//	@Override
-//	public String toString() {
-//		return "ObjectJSON [response_code=" + response_code + ", results=" + Arrays.toString(results) + "]";
-//	}
-	
-	
-	
-	
 
 }
