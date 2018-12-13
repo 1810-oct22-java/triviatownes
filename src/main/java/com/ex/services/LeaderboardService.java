1b616b820979abb8709b4a49230f379e20cb1949
Ian Barney <ianbarney23@gmail.com>
2018-12-13 09:21:37 -0500

    leaderboardservice save

--- src/main/java/com/ex/services/LeaderboardService.java
+++ src/main/java/com/ex/services/LeaderboardService.java
@@ -1,23 +1,20 @@
 package com.ex.services;
 
-import java.util.List;
+import java.util.ArrayList;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
 
-import com.ex.beans.game.HighScorePlayerBean;
-import com.ex.repository.UserRepository;
+import com.ex.beans.game.PlayerBean;
 
 
 
+
+@Service("LeaderBoardService")
 public class LeaderBoardService {
-	ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-	UserRepository repo = (UserRepository) context.getBean(UserRepository.class);
-	public List<HighScorePlayerBean> getAll(){
-		return repo.findAll();
-	}
+
+	static ArrayList<PlayerBean> players = new ArrayList<PlayerBean>();
+		
 	
-	public HighScorePlayerBean save(HighScorePlayerBean highScore) {
-		return repo.save(highScore);
-	}
+
+	
 }
